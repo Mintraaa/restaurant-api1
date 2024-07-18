1412e4855ab01cf1f0b33829e5a870d182700211
Mintraaa <111554748+Mintraaa@users.noreply.github.com>
2024-07-18 09:21:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const restaurantRouter = require("./routers/restaurant.router");
+const authRouter = require("./routers/auth.router");
+const db = require("./models/")
+const role = db.Role;
+
+//Dev mode
+/*db.sequelize.AsyncQueueError({force:true}).then(()=>{
+    initRole();
+    console.log("Drop and Sync DB")
+})*/
+
+const initRole = () => {
+    role.create({ id:1, name:"user"});
+    role.create({ id:2, name: "moderator" });
+    role.create({ id:3, name: "admin" });
+};
+
+//use middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//use router
+app.use("/api/v1/restaurants", restaurantRouter);
+app.use("/api/v1/auth", authRouter);
+
+
+app.get('/', (req,res) => {
+    res.send('<h1>Hello Restaurant API</h1>');
+});
+
+app.listen(PORT, ()=>{
+    console.log("Listening to http://localhost:"+PORT);
+});
